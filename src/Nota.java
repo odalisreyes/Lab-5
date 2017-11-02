a5edc28335db5ee46e9e72d8ddbfa58bd692ed13
odalisreyes <reyesgoe@gmail.com>
2017-11-02 08:48:14 -0600

    Comentarios de la clase Nota

--- src/Nota.java
+++ src/Nota.java
@@ -1,5 +1,16 @@
+/*
+ * @Authors: Javier Anleu 17149
+ * 			Odalis Reyes 17032
+ * @File name: Nota.java
+ * @Project and date: Laboratorio 5 (2/11/2017)
+ * 
+ * PROPÓSITO: Es una interfaz y tiene como propósito implementar los métodos a las demás clases que estén 
+ * 			  relacionadas con esta mediante polimorfismo.
+ */
+
 
 public interface Nota {
 
+	/* Es un método abstracto que servirá para calcular las notas del escalafón */
 	public void calcular();
 }
