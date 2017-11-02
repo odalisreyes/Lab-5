273eb3cfd1e40599d3d89b9f4bf2f1bcbc0ca487
Unknown <reyesgoe@gmail.com>
2017-11-02 17:58:16 -0600

    Clase Bachelor
    Tiene el método de calcular el promedio de escalafon para los aspirantes de Bachillerato

---
+++ src/Bachelor.java
@@ -0,0 +1,51 @@
+import java.text.DecimalFormat;
+
+/**
+ * PROPOSITO:
+ * 
+ * @author Javier Anleu 17149
+ * @author Odalis Reyes 17032
+ * @version 1.0
+ * @since November 2nd, 2017
+ *
+ */
+public class Bachelor extends Aspirer implements Nota {
+
+
+	/**
+	 * Constructor de la clase. Hereda los atributos de la clase padre.
+	 * 
+	 * @param name Nombre del aspirante
+	 * @param dPI Número de DPI
+	 * @param average Promedio de las clases de 4to y 5to Bachillerato
+	 * @param type Tipo de aspirante
+	 * @param desvinculado El aspirante es desvinculado de Bachillerato o no
+	 * @param notaHistoria Nota del examen de historia
+	 */
+	public Bachelor(String name, String dPI, double average, boolean type, boolean desvinculado, double notaHistoria) {
+		super(name, dPI, average, type, desvinculado, notaHistoria);
+	}
+
+	
+	/*
+	 * MÉTODO: Tiene como objetivo calcular el promedio del escalafon
+	 */
+	@Override
+	public void calcular() {
+		if (Desvinculado == false) {
+			double Promedio = (Average*0.6) + (NotaHistoria*0.4);
+			DecimalFormat decimal = new DecimalFormat(".##");
+			String NotaEscalafon = decimal.format(Promedio);
+			Escalafon = Double.parseDouble(NotaEscalafon);
+		} 
+		
+		else {
+			double Promedio = (Average*0.4) + (NotaHistoria*0.6);
+			DecimalFormat decimal = new DecimalFormat(".##");
+			String NotaEscalafon = decimal.format(Promedio);
+			Escalafon = Double.parseDouble(NotaEscalafon);
+		}
+		
+	
+
+} }
