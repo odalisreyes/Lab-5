7e0e33ed5753a42ff9f529a45e33f02e099a39cf
DoctorStrange <anl17149@uvg.edu.gt>
2017-11-02 08:51:48 -0600

    Modificaciones a los comentarios de la interfaz
    Se agregaron los tags para los autores, versión y fecha

--- src/Nota.java
+++ src/Nota.java
@@ -1,14 +1,12 @@
-/*
- * @Authors: Javier Anleu 17149
- * 			Odalis Reyes 17032
- * @File name: Nota.java
- * @Project and date: Laboratorio 5 (2/11/2017)
- * 
+/**
  * PROPÓSITO: Es una interfaz y tiene como propósito implementar los métodos a las demás clases que estén 
  * 			  relacionadas con esta mediante polimorfismo.
+ * @author Javier Anleu 17149
+ * @author Odalis Reyes 17032
+ * @version 1.0
+ * @since November 2nd, 2017
+ *
  */
-
-
 public interface Nota {
 
 	/* Es un método abstracto que servirá para calcular las notas del escalafón */
