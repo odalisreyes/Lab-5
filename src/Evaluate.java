e66df036f39239f5508c5b028cefb6d752b6d1b0
DoctorStrange <anl17149@uvg.edu.gt>
2017-11-02 20:27:27 -0600

    Intento db
    Intento fallido de la base de datos

--- src/Evaluate.java
+++ src/Evaluate.java
@@ -1,3 +1,4 @@
+import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 /**
@@ -7,6 +8,12 @@ import org.mongodb.morphia.Morphia;
 public class Evaluate {
 	
 	final Morphia morphia = new Morphia();
-	
 
+	// tell Morphia where to find your classes
+	// can be called multiple times with different packages or classes
+	morphia.mapPackage("org.mongodb.morphia.example");
+
+	// create the Datastore connecting to the default port on the local host
+	final Datastore datastore = morphia.createDatastore(new MongoClient(), "morphia_example");
+	datastore.ensureIndexes();
 }
