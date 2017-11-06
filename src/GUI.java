2409d308b4de5726506237d90110e6087be04d5f
Unknown <reyesgoe@gmail.com>
2017-11-06 12:50:54 -0600

    Clase controladora y FXML del GUI
    Creación de clases para el GUI

---
+++ src/GUI.java
@@ -0,0 +1,148 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class GUI {
+
+	@FXML // fx:id="no"
+	private RadioButton no; // Value injected by FXMLLoader
+
+	@FXML // fx:id="mostrarPosicion"
+	private Button mostrarPosicion; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tipo"
+	private Label tipo; // Value injected by FXMLLoader
+
+	@FXML // fx:id="notaPromedio"
+	private TextField notaPromedio; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tDatosAspi"
+	private Label tDatosAspi; // Value injected by FXMLLoader
+
+	@FXML // fx:id="labelNewDato"
+	private Label labelNewDato; // Value injected by FXMLLoader
+
+	@FXML // fx:id="labelModificar"
+	private Label labelModificar; // Value injected by FXMLLoader
+
+	@FXML // fx:id="notaAptitud"
+	private TextField notaAptitud; // Value injected by FXMLLoader
+
+	@FXML // fx:id="labelPosicion"
+	private Label labelPosicion; // Value injected by FXMLLoader
+
+	@FXML // fx:id="opcionTipo"
+	private ComboBox<?> opcionTipo; // Value injected by FXMLLoader
+
+	@FXML // fx:id="notaMate"
+	private TextField notaMate; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tMostrar"
+	private Label tMostrar; // Value injected by FXMLLoader
+
+	@FXML // fx:id="ingresarMayorA"
+	private TextField ingresarMayorA; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tEspanol"
+	private Label tEspanol; // Value injected by FXMLLoader
+
+	@FXML // fx:id="si"
+	private RadioButton si; // Value injected by FXMLLoader
+
+	@FXML // fx:id="THistoria"
+	private Label THistoria; // Value injected by FXMLLoader
+
+	@FXML // fx:id="TMatematica"
+	private Label TMatematica; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tPromMayor"
+	private Label tPromMayor; // Value injected by FXMLLoader
+
+	@FXML // fx:id="agregarAspirante"
+	private Button agregarAspirante; // Value injected by FXMLLoader
+
+	@FXML // fx:id="ingresarPosicion"
+	private TextField ingresarPosicion; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tExamenes"
+	private Label tExamenes; // Value injected by FXMLLoader
+
+	@FXML // fx:id="notaHistoria"
+	private TextField notaHistoria; // Value injected by FXMLLoader
+
+	@FXML // fx:id="labelRespuesta2"
+	private Label labelRespuesta2; // Value injected by FXMLLoader
+
+	@FXML // fx:id="opcionModiciar"
+	private ComboBox<?> opcionModiciar; // Value injected by FXMLLoader
+
+	@FXML // fx:id="labelRespuesta1"
+	private Label labelRespuesta1; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tPosicionEscal"
+	private Label tPosicionEscal; // Value injected by FXMLLoader
+
+	@FXML // fx:id="labelMayorA"
+	private Label labelMayorA; // Value injected by FXMLLoader
+
+	@FXML // fx:id="TnombreAspirante"
+	private Label TnombreAspirante; // Value injected by FXMLLoader
+
+	@FXML // fx:id="TDesvin"
+	private Label TDesvin; // Value injected by FXMLLoader
+
+	@FXML // fx:id="botonAspirantes"
+	private Button botonAspirantes; // Value injected by FXMLLoader
+
+	@FXML // fx:id="notaEspanol"
+	private TextField notaEspanol; // Value injected by FXMLLoader
+
+	@FXML // fx:id="TPromedio"
+	private Label TPromedio; // Value injected by FXMLLoader
+
+	@FXML // fx:id="todosAspirantes"
+	private TextArea todosAspirantes; // Value injected by FXMLLoader
+
+	@FXML // fx:id="botonReiniciar"
+	private Button botonReiniciar; // Value injected by FXMLLoader
+
+	@FXML // fx:id="ingresarNumEscal"
+	private TextField ingresarNumEscal; // Value injected by FXMLLoader
+
+	@FXML // fx:id="botonCalcular2"
+	private Button botonCalcular2; // Value injected by FXMLLoader
+
+	@FXML // fx:id="botonCalcular1"
+	private Button botonCalcular1; // Value injected by FXMLLoader
+
+	@FXML // fx:id="botonModificar"
+	private Button botonModificar; // Value injected by FXMLLoader
+
+	@FXML // fx:id="tAptitud"
+	private Label tAptitud; // Value injected by FXMLLoader
+
+	@FXML // fx:id="mostrarAspirante"
+	private TextArea mostrarAspirante; // Value injected by FXMLLoader
+
+	@FXML // fx:id="ingresarNewDato"
+	private TextField ingresarNewDato; // Value injected by FXMLLoader
+
+	@FXML // fx:id="nomAspirante"
+	private TextField nomAspirante; // Value injected by FXMLLoader
+
+	@FXML
+	void bf0404(ActionEvent event) {
+
+	}
+
+	@FXML
+	void bf0404(ActionEvent event) {
+
+	}
+
+}
